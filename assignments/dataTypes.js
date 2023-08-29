979865117b1ffe52e16fa797b923b4b906451541
Noseilos <jonaseguiron@gmail.com>
2023-08-29 13:36:01 +0800

    Data Types

---
+++ assignments/dataTypes.js
@@ -0,0 +1,9 @@
+let isIsland = true;
+let language;
+let population = '113.9 million';
+let country = 'Philippines';
+
+console.log('Is Island: ' + isIsland);
+console.log('Population: ' + population);
+console.log('Country: ' + country);
+console.log('Language: ' + language);
