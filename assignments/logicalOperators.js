a8cf647f48c5b3c00fcb4015ef116e5e0e3343b2
Noseilos <jonaseguiron@gmail.com>
2023-08-29 22:26:16 +0800

    Logical Operators

---
+++ assignments/logicalOperators.js
@@ -0,0 +1,9 @@
+let speaksEnglish = true;
+let notIsland = false;
+let rightPopulation = true;
+
+if (speaksEnglish && notIsland && rightPopulation){
+    console.log('You should live in Philippines!');
+} else {
+    console.log('Philippines does not meet your criteria :(')
+}
