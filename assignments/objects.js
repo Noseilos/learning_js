0d3c1800c9491edf51d0c1b107cd0029cd577d2b
Noseilos <jonaseguiron@gmail.com>
2023-09-08 03:17:12 +0800

    objects

---
+++ assignments/objects.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/*
+
+LECTURE: Introduction to Objects
+1. Create an object called 'myCountry' for a country of your choice, containing 
+properties 'country', 'capital', 'language', 'population' and 
+'neighbours' (an array like we used in previous assignments
+
+*/
+
+const myCountry = {
+    country: 'Philippines',
+    capital: 'Manila',
+    language: 'Tagalog',
+    population: '109.04 million',
+    neighbours: ['China', 'USA', 'Taiwan'],
+};
+
+console.log(myCountry);
