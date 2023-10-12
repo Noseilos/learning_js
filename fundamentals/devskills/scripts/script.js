8e6d94b19fd5a154c339bb13f3188f583a4e68f7
Noseilos <jonaseguiron@gmail.com>
2023-10-13 00:15:48 +0800

    added devskills

--- fundamentals/devskills/scripts/script.js
+++ fundamentals/devskills/scripts/script.js
@@ -1,34 +1,50 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
-const temparatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+// const temparatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 
 
-const calcTempAmplitudeBug = function (temp) {
+// const calcTempAmplitudeBug = function (temp) {
 
-    // introduces bug due to zero being lower than other values in the array
-    let max = 0;
-    let min = 0;
-    let amp;
+//     // introduces bug due to zero being lower than other values in the array
+//     let max = 0;
+//     let min = 0;
+//     let amp;
 
-    for(let i = 0; i < temp.length; i++){
+//     for(let i = 0; i < temp.length; i++){
 
-        if(temp[i] > max){
+//         if(temp[i] > max){
 
-            max = temp[i];
+//             max = temp[i];
 
-        } else if (temp[i] < min){
+//         } else if (temp[i] < min){
 
-            min = temp[i];
+//             min = temp[i];
             
-        };
+//         };
 
-    };
+//     };
     
-    console.log(min, max);
+//     console.log(min, max);
 
-    if(min >= 0) return amp = max - min;
-    if(min < 0) return amp = max + min;
+//     if(min >= 0) return amp = max - min;
+//     if(min < 0) return amp = max + min;
+    
+// };
+
+// console.log(calcTempAmplitudeBug(temparatures));
+
+const maxTemp = [17, 21, 23];
+const maxTemp2 = [12, 5, -5, 0, 4];
+
+const printForecast = function(arr){
+    
+    let mes = "... ";
+    for(let i = 0; i < arr.length; i++){
+        mes += `${arr[i]}°C in ${i + 1} days ... `
+    };
+    console.log(mes);
+
     
 };
 
-console.log(calcTempAmplitudeBug(temparatures));
+printForecast(maxTemp2);
