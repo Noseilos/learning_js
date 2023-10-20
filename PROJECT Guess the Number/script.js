416ed88cb5e2145921f4285dec360b44fee187cd
Noseilos <jonaseguiron@gmail.com>
2023-10-20 16:44:25 +0800

    manipulating css styles

--- PROJECT Guess the Number/script.js
+++ PROJECT Guess the Number/script.js
@@ -33,6 +33,79 @@
 
 // ---------------- START Implementing the Game Logic ---------------- //
 
+// let newNumber = Math.trunc(Math.random() * 20) + 1;
+// let score = 20;
+
+// const evaluateGuess = function(){
+//     const guess = Number(document.querySelector('.guess').value);
+//     console.log(guess, newNumber);
+
+//     if (!guess || guess === 0) {
+
+//         document.querySelector('.message').textContent = `⛔⛔ Enter Number ⛔⛔`;
+//         score--;
+//         document.querySelector('.score').textContent = score;
+//         if (score === 0){
+//             document.querySelector('.message').textContent = `You Lost 🥲`;
+//         } else {
+            
+//         }
+
+//     } else if (guess < newNumber){
+
+//         if(newNumber - guess >= 5){
+//             if (score === 0){
+//                 document.querySelector('.message').textContent = `You Lost 🥲`;
+//             } else {
+//                 document.querySelector('.message').textContent = `Too Low! Guess Higher`;
+//                 score--;
+//                 document.querySelector('.score').textContent = score;
+//             }
+//         } else {
+//             if (score === 0){
+//                 document.querySelector('.message').textContent = `You Lost 🥲`;
+//             } else {
+//                 document.querySelector('.message').textContent = `Guess Higher`;
+//                 score--;
+//                 document.querySelector('.score').textContent = score;
+//             }
+//         }
+        
+
+//     } else if (guess > newNumber) {
+
+//         if(guess - newNumber >= 5){
+//             if (score === 0){
+//                 document.querySelector('.message').textContent = `You Lost 🥲`;
+//             } else {
+//                 document.querySelector('.message').textContent = `Too High! Guess Lower`;
+//                 score--;
+//                 document.querySelector('.score').textContent = score;
+//             }
+//         } else {
+//             if (score === 0){
+//                 document.querySelector('.message').textContent = `You Lost 🥲`;
+//             } else {
+//                 document.querySelector('.message').textContent = `Guess Lower`;
+//                 score--;
+//                 document.querySelector('.score').textContent = score;
+//             }
+//         }
+
+//     } else if (guess === newNumber) {
+
+//         document.querySelector('.message').textContent = `🎉🎉 You Guessed It! 🎉🎉`;
+//         document.querySelector('.number').textContent = newNumber;
+
+//     }
+// }
+
+// document.querySelector('.check').addEventListener('click', evaluateGuess);
+
+// ---------------- END Implementing the Game Logic ---------------- //
+
+// ---------------- START Manipulating CSS Styles ---------------- //
+
 let newNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
@@ -94,6 +167,9 @@ const evaluateGuess = function(){
 
     } else if (guess === newNumber) {
 
+        document.querySelector('body').style.backgroundColor = '#64CCC5';
+        document.querySelector('.number').style.width = '30rem';
+        document.querySelector('.number').style.borderRadius = '20%';
         document.querySelector('.message').textContent = `🎉🎉 You Guessed It! 🎉🎉`;
         document.querySelector('.number').textContent = newNumber;
 
@@ -102,4 +178,4 @@ const evaluateGuess = function(){
 
 document.querySelector('.check').addEventListener('click', evaluateGuess);
 
-// ---------------- END Implementing the Game Logic ---------------- //
+// ---------------- END Manipulating CSS Styles ---------------- //
