ce864543d7fa8090f7bb24e7aff746dc5969a398
Noseilos <jonaseguiron@gmail.com>
2023-10-24 02:18:09 +0800

    added hold function and switch player

--- PROJECT Dice of Greed/script.js
+++ PROJECT Dice of Greed/script.js
@@ -22,34 +22,59 @@ diceEl.classList.add('hidden');
 const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true;
+
+const switchPlayer = function() {
+    currentScore = 0;
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+    
+    activePlayer = activePlayer === 0 ? 1 : 0;
+
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+}
 
 // Roll dice function
 btnRoll.addEventListener('click', function(){
-    // Generate random number
-    const dice = Math.trunc(Math.random() * 6) + 1;
-    console.log(dice);
+    if(playing){
+        // Generate random number
+        const dice = Math.trunc(Math.random() * 6) + 1;
 
-    // Display dice
-    diceEl.classList.remove('hidden');
-    diceEl.src = `./images/dice-${dice}.png`;
+        // Display dice
+        diceEl.classList.remove('hidden');
+        diceEl.src = `./images/dice-${dice}.png`;
 
-    // Check if rolled 1: if true 
-    if(dice !== 1){
-        // add dice to current score
-        currentScore += dice;
-        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+        // Check if rolled 1: if true 
+        if(dice !== 1){
+            // add dice to current score
+            currentScore += dice;
+            document.getElementById(`current--${activePlayer}`).textContent = currentScore;
 
-    } else {
-        // switch to next player
-        currentScore = 0;
-        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
-        
-        activePlayer = activePlayer === 0 ? 1 : 0;
+        } else {
+            // switch to next player
+            switchPlayer();
 
-        player0El.classList.toggle('player--active');
-        player1El.classList.toggle('player--active');
-
+        }
     }
+});
 
-    console.log(`active: ` + activePlayer);
+// Hold points function
+btnHold.addEventListener('click', function(){
+    if(playing){
+        // Add score to active player
+        scores[activePlayer] += currentScore;
+        document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+
+        // Check if score is >= 100
+        if(scores[activePlayer] >= 10){
+            // finish the game
+            playing = false;
+            diceEl.classList.add('hidden');
+            document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+            document.querySelector(`.player--${activePlayer}`).classList.remove('player--active')
+        } else {
+            // Switch to next player
+            switchPlayer();
+        }
+    }
 })
