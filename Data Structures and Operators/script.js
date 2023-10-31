c6b0dad2f4cc1eb8988ba0a7c069d95dee1c6435
Noseilos <jonaseguiron@gmail.com>
2023-10-31 20:09:55 +0800

    challenge1

--- Data Structures and Operators/script.js
+++ Data Structures and Operators/script.js
@@ -50,29 +50,135 @@ const rest1 = {
   numGuests: 0,
 };
 
-const rest2 = {
-  name: 'La Piazza',
-  owner: 'Giovanni Rossi',
+/* 
+
+CODING CHALLENGE 
+
+We're building a football betting app 
+
+Suppose we get data from a web service about a certain game. In this challenge
+we're gonna work with the data. So here are your tasks:
+
+1. Create one player array for each team (variables 'players1' and 'players2')
+2. The first player in any player array is the goalkeeper and the others are field players.
+   For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one 
+   array ('fieldPlayers') with all the remaining 10 field players.
+3. Create an array 'allPlayers' containing all players of both teams (22 players)
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array 
+   ('playersFinal') containing all the original team1 players plus 'Thiago', 'Coutinho' and
+   'Perisic'.
+5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw'
+   'team2').
+6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT
+   an array) and prints each of them to the console, along with the number of goals who were
+   scored (number of player names passed in)
+7. The team with the lower odd is more likely to win. Print to the console which team is more
+   likely to win, WITHOUT using an if/else statement or the ternary operator.
+
+TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then call the
+                 function again with the players from game.scored
+
+*/
+
+
+
+const game = {
+
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer', 
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witzel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  data: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+
 };
 
-// OR assignment operator
-// rest1.numGuests = rest1.numGuests || 10;
-// rest2.numGuests = rest2.numGuests || 10;
-// rest1.numGuests ||= 11;
-// rest2.numGuests ||= 11;
+const [ players1, players2 ] = [ game.players[0], game.players[1] ];
+console.log(`Team 1: `, players1); 
+console.log(`Team 2: `, players2); 
 
-// Nullish assignment operator (null or undefined)
-rest1.numGuests ??= 11;
-rest2.numGuests ??= 11;
+const gk  = players1[0];
+const [ , ...fieldPlayers] = [ ...players1 ]
+console.log( 'Goal Keeper: ', gk);
+console.log( 'Field Players: ', fieldPlayers);
 
-// AND assignment operator
-// rest1.owner = rest1.owner && '<ANONYMOUS>';
-// rest2.owner = rest2.owner && '<ANONYMOUS>';
-rest1.owner &&= '<ANONYMOUS>';
-rest2.owner &&= '<ANONYMOUS>';
+const allPlayers = [ ...players1, ...players2 ];
+console.log( 'All Players: ', allPlayers);
 
-console.log(rest1);
-console.log(rest2);
+const playersFinal = [ ...players1, 'Thiago', 'Countinho', 'Perisic' ];
+console.log( 'Final Players: ', playersFinal);
+
+const { team1, x: draw, team2 } = game.odds;
+console.log( 'Game odds: ', team1, draw, team2);
+
+const printGoals = function(playerNames){
+
+  for(let i = 0; i < playerNames; i++) console.log(game.scored[i]);
+  console.log(playerNames);
+
+};
+
+printGoals(game.scored.length);
+
+console.log( (game.odds.team2 < game.odds.x) && (game.odds.team2 < game.odds.team1) && 'Team 2 wins' || 
+             (game.odds.x < game.odds.team1) && (game.odds.x < game.odds.team2) && 'Draw' ||
+             (game.odds.team1 < game.odds.x) && (game.odds.team1 < game.odds.team2) && 'Team 1 wins');
+
+// const rest2 = {
+//   name: 'La Piazza',
+//   owner: 'Giovanni Rossi',
+// };
+
+// // OR assignment operator
+// // rest1.numGuests = rest1.numGuests || 10;
+// // rest2.numGuests = rest2.numGuests || 10;
+// // rest1.numGuests ||= 11;
+// // rest2.numGuests ||= 11;
+
+// // Nullish assignment operator (null or undefined)
+// rest1.numGuests ??= 11;
+// rest2.numGuests ??= 11;
+
+// // AND assignment operator
+// // rest1.owner = rest1.owner && '<ANONYMOUS>';
+// // rest2.owner = rest2.owner && '<ANONYMOUS>';
+// rest1.owner &&= '<ANONYMOUS>';
+// rest2.owner &&= '<ANONYMOUS>';
+
+// console.log(rest1);
+// console.log(rest2);
 
 // restaurant.numGuests = 0;
 // const guest1 = restaurant.numGuests ? restaurant.numGuests : 10;
