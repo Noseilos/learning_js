6db27ef4c4ffdb85ca0afac01baf894039923d73
Noseilos <jonaseguiron@gmail.com>
2023-12-04 01:06:07 +0800

    working with strings part 3

--- Data Structures and Operators/workingWithStrings.js
+++ Data Structures and Operators/workingWithStrings.js
@@ -36,65 +36,115 @@
 
 // --------------------------- PART 2 ---------------------------
 
-const airline = 'TAP Air Portugal';
+// const airline = 'TAP Air Portugal';
 
-console.log(airline.toLowerCase())
-console.log(airline.toUpperCase())
+// console.log(airline.toLowerCase())
+// console.log(airline.toUpperCase())
 
-// fix capitalization in name
+// // fix capitalization in name
 
-const passenger = 'doRaEMon';
+// const passenger = 'doRaEMon';
 
-const passengerLower = passenger.toLowerCase();
-const passengerCorrect = passengerLower[0].toUpperCase() + passengerLower.slice(1);
-console.log(passengerCorrect);
+// const passengerLower = passenger.toLowerCase();
+// const passengerCorrect = passengerLower[0].toUpperCase() + passengerLower.slice(1);
+// console.log(passengerCorrect);
 
-// compairing email
+// // compairing email
 
-const email = 'hello@jeers.com';
-const loginEmail = 'HellO@jeers.com \n'
+// const email = 'hello@jeers.com';
+// const loginEmail = 'HellO@jeers.com \n'
 
-const lowerEmail = loginEmail.toLowerCase();
-const trimmedEmail = lowerEmail.trim()
-console.log(trimmedEmail);
+// const lowerEmail = loginEmail.toLowerCase();
+// const trimmedEmail = lowerEmail.trim()
+// console.log(trimmedEmail);
 
-const normalizeEmail = loginEmail.toLowerCase().trim()
-console.log(normalizeEmail);
+// const normalizeEmail = loginEmail.toLowerCase().trim()
+// console.log(normalizeEmail);
 
-console.log(email === normalizeEmail);
+// console.log(email === normalizeEmail);
 
-// replacing
+// // replacing
 
-const pricePH = '₱288,987';
-const priceUS = pricePH.replace('₱', '$').replace(',', '.')
-console.log(priceUS)
+// const pricePH = '₱288,987';
+// const priceUS = pricePH.replace('₱', '$').replace(',', '.')
+// console.log(priceUS)
 
-const announcement = 'All passengers come to boarding door 23. Boarding door 23!';
-console.log(announcement.replaceAll('door', 'gate'));
+// const announcement = 'All passengers come to boarding door 23. Boarding door 23!';
+// console.log(announcement.replaceAll('door', 'gate'));
 
-console.log(announcement.replaceAll(/door/g, 'gate'));
+// console.log(announcement.replaceAll(/door/g, 'gate'));
 
-// booleans 
+// // booleans 
 
-const plane = 'Airbus A320neo';
-console.log(plane.includes('A320'));
-console.log(plane.startsWith('Air'));
+// const plane = 'Airbus A320neo';
+// console.log(plane.includes('A320'));
+// console.log(plane.startsWith('Air'));
 
-if(plane.startsWith('Airbus') && plane.endsWith('neo')){
-    console.log('Part of the Airbus family')
-}
+// if(plane.startsWith('Airbus') && plane.endsWith('neo')){
+//     console.log('Part of the Airbus family')
+// }
 
-// practice exercise
+// // practice exercise
 
-const checkBaggage = function(items){
-    const baggage = items.toLowerCase();
-    if (baggage.includes('knife') || baggage.includes('gun')) {
-        console.log('You are not allowed on board')
-    } else {
-        console.log('Welcome aboard!')
+// const checkBaggage = function(items){
+//     const baggage = items.toLowerCase();
+//     if (baggage.includes('knife') || baggage.includes('gun')) {
+//         console.log('You are not allowed on board')
+//     } else {
+//         console.log('Welcome aboard!')
+//     }
+// }
+
+// checkBaggage('I have a laptop a Food and a pocket Knife');
+// checkBaggage('Socks and camera');
+// checkBaggage('Got some snacks and gun for protection');
+
+// --------------------------- PART 3 ---------------------------
+
+// split and join
+console.log('a+very+nice+day'.split('+'));
+
+const [firstName, lastName] = 'Jonas Eguiron'.split(' ');
+
+const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ')
+console.log(newName);
+
+const capitalizeName = function(name){
+    const names = name.split(' ');
+    const namesUpper = [];
+
+    for(const n of names){
+        namesUpper.push(n[0].toUpperCase() + n.slice(1))
     }
+
+    console.log(namesUpper.join(' '));
+}
+capitalizeName('jessica ann smith davis');
+capitalizeName('jonas joseph serra eguiron');
+
+// padding
+
+const message = 'go to gate 25';
+console.log(message.padStart(25, '+').padEnd(30, '+'));
+
+
+const maskCreditCart = function(number){
+    const str = number + '';
+    const last = str.slice(-4);
+    return last.padStart(str.length, '*');
 }
 
-checkBaggage('I have a laptop a Food and a pocket Knife');
-checkBaggage('Socks and camera');
-checkBaggage('Got some snacks and gun for protection');
+console.log(maskCreditCart(3455672341));
+console.log(maskCreditCart('3446671298'));
+
+// repeat
+
+const messageWeather = 'Bad weather, all departures are delayed... ';
+console.log(messageWeather.repeat(5))
+
+const planesInLine = function(n){
+    console.log(`There are ${n} planes in line ${'✈️'.repeat(n)}`);
+}
+
+planesInLine(3);
+planesInLine(7);
