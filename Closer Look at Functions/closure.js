03cbf3f870dfe9e051060db257cc90e14fd8e730
Noseilos <jonaseguiron@gmail.com>
2023-12-11 23:58:53 +0800

    closures cont.

--- Closer Look at Functions/closure.js
+++ Closer Look at Functions/closure.js
@@ -1,22 +1,67 @@
 'use strict';
 
-const countStudents = function(){
-    let student = 0;
+// const countStudents = function(){
+//     let student = 0;
 
-    return function(){
-        student++
-        console.log(`${student} student registered`)
+//     return function(){
+//         student++
+//         console.log(`${student} student registered`)
+//     }
+// }
+
+// console.dir(countStudents)
+// // Look for [[scope]] in console
+
+// const registrar = countStudents();
+
+// registrar();
+// registrar();
+// registrar();
+
+// console.dir(registrar);
+// // Look for [[scope]] in console
+
+
+// ------------------- CONTINUATION -------------------
+
+let f;
+
+const g = function(){
+    const a = 23;
+    f = function(){
+        console.log(a * 2);
     }
 }
 
-console.dir(countStudents)
-// Look for [[scope]] in console
 
-const registrar = countStudents();
+const h = function(){
+    const b = 123;
+    f = function(){
+        console.log(b * 2);
+    }
+}
 
-registrar();
-registrar();
-registrar();
+g();
+f();
+console.dir(f);
 
-console.dir(registrar);
-// Look for [[scope]] in console
+// Re-assigning f function
+
+h();
+f();
+console.dir(f);
+
+// Example 2 ------
+
+const boardPassengers = function(n, wait){
+    const perGroup = n / 3;
+
+    setTimeout(function(){
+        console.log(`We are now boarding all ${n} passengers`)
+        console.log(`There are 3 groups, each with ${perGroup} passengers`)
+    }, wait * 1000)
+
+    console.log(`Will start boarding in ${wait} seconds`);
+}
+
+boardPassengers(180, 3);
