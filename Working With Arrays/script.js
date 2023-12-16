c34e939a25c57a79d05d2cfcb773ee6197b1bc19
Noseilos <jonaseguiron@gmail.com>
2023-12-16 20:53:56 +0800

    map method

--- Working With Arrays/script.js
+++ Working With Arrays/script.js
@@ -230,7 +230,7 @@ TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
 
 GOOD LUCK 😀
 */
-
+ /*
 const checkDogs = function(dogsJulia, dogsKate){
   const dogsJuliaCorrected = dogsJulia.slice();
   dogsJuliaCorrected.splice(0, 1);
@@ -250,8 +250,38 @@ const checkDogs = function(dogsJulia, dogsKate){
 }
 
 checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3])
+*/
 
 
 
 
+
+// --------------- THE MAP METHOD ---------------
+
+
+
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const eurToPhp = 61.12;
+
+const movementsPhp = movements.map(mov => eurToPhp * mov)
+
+console.log(movements)
+console.log(movementsPhp)
+
+const movementsPhpFor = []
+for(const mov of movements){
+  movementsPhpFor.push(mov * eurToPhp);
+}
+
+console.log(movementsPhpFor);
+
+const movementDescription = movements.map((mov, i) => {
+
+  `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(mov)}`
+
+});
+
+console.log(movementDescription);
 
